e7935f553ff0f895e42c9b534c4c64694221334c
Igor Kalinovskii <igor.kalinovskii@developex.local>
2019-10-04 15:28:17 +0300

    проапдейтил 5ю задачу

--- T5.py
+++ T5.py
@@ -2,12 +2,9 @@ a = input('Введите число А\n')
 b = input('Введите число Б\n')
 
 try:
-    res = int(a) + int(b)
+    res = float(a) + float(b)
 except ValueError:
-    try:
-        res = float(a) + float(b)
-    except ValueError:
-        res = str(a) + str(b)
+    res = str(a) + str(b)
 
 print('Результат', res)
 print('Тип результата:', type(res))
